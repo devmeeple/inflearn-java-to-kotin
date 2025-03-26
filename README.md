6e71d32be2ebf6b4560941b0ce4bd51073cb3c2a
devmeeple <devmeeple@gmail.com>
2025-03-26 18:42:09 +0900

    [2-8] 유저 관련 기능 테스트 작성하기

--- README.md
+++ README.md
@@ -37,4 +37,23 @@ results[0].age must not be null
 ```
 
 - 코틀린은 자바와 상호 운용성을 고려하여 설계되었기 때문에 자바 코드를 호출할 수 있다.
-- 차이에서 발생하는 느슨함을 표현하는 타입이다.  
+- 차이에서 발생하는 느슨함을 표현하는 타입이다.
+
+## 2.8 유저 관련 기능 테스트 작성하기
+
+### 2.8.1 테스트 독립성 유지하기
+
+`@AfterEach`와 인메모리 데이터베이스, `@Transactional` 외에 독립성을 유지하는 방법을 알아보자.
+
+- [Testcontainers를 이용한 테스트 환경 구축하기](https://dealicious-inc.github.io/2022/01/10/test-containers.html)
+
+### 2.8.2 AssertJ Assertions
+
+```kotlin
+assertThat(results).extracting("name").containsExactlyInAnyOrder("김동영", "최용준")
+assertThat(results).extracting("age").containsExactlyInAnyOrder(29, null)
+```
+
+- `containsExactlyInAnyOrder`: 순서와 상관없이 컬렉션에 정확히 포함하고 있는지 검증한다.
+
+- [AssertJ - fluent assertions java library](https://assertj.github.io/doc/#assertj-core-assertions-guide())
--- src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -1,8 +1,12 @@
 package com.group.libraryapp.service.user
 
+import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -13,6 +17,12 @@ class UserServiceTest @Autowired constructor(
     private val userRepository: UserRepository
 ) {
 
+    @AfterEach
+    fun clean() {
+        userRepository.deleteAll()
+    }
+
+    @DisplayName("사용자를 등록한다")
     @Test
     fun saveUserTest() {
         val request = UserCreateRequest("Martin Fowler", null)
@@ -23,5 +33,46 @@ class UserServiceTest @Autowired constructor(
         assertThat(results.size).isEqualTo(1)
         assertThat(results[0].name).isEqualTo("Martin Fowler")
         assertThat(results[0].age).isNull()
+    }
+
+    @DisplayName("등록한 사용자를 전체 조회한다")
+    @Test
+    fun getUserTest() {
+        userRepository.saveAll(
+            listOf(
+                User("김동영", 29),
+                User("최용준", null),
+            )
+        )
+
+        val results = userService.getUsers()
+
+        assertThat(results).hasSize(2)
+        assertThat(results).extracting("name").containsExactlyInAnyOrder("김동영", "최용준")
+        assertThat(results).extracting("age").containsExactlyInAnyOrder(29, null)
+    }
+
+    @DisplayName("사용자 이름을 수정한다")
+    @Test
+    fun updateUserNameTest() {
+        val savedUser = userRepository.save(User("문지수", 30))
+        val request = UserUpdateRequest(savedUser.id, "박창현")
+
+        userService.updateUserName(request)
+
+        val result = userRepository.findAll()[0]
+        assertThat(result.name).isEqualTo("박창현")
+    }
+
+    @DisplayName("사용자를 삭제한다")
+    @Test
+    fun deleteUserTest() {
+        userRepository.save(User("양인규", 29))
+
+        userService.deleteUser("양인규")
+
+        assertThat(userRepository.findAll()).isEmpty()
+
+
     }
 }
